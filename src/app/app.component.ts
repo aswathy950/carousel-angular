90bb1b6fa8a580a7b99c176ee0da3419ba50b79a
aswathyb950 <aswathybalan950@gmail.com>
2021-04-19 16:38:18 +0530

    imgae url update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,61 +23,61 @@ export class AppComponent implements OnInit{
 
   getCarouselItems() {
     let carouselItems = [{
-      itemImage: "../assets/images/item-1.jpg",
+      itemImage: "assets/images/item-1.jpg",
       itemName: "Item 1",
       itemprice: "1001",
       itemCategory: "Category 1"
     },
     {
-      itemImage: "../assets/images/item-2.jpg",
+      itemImage: "assets/images/item-2.jpg",
       itemName: "Item 2",
       itemprice: "1002",
       itemCategory: "Category 2"
     },
     {
-      itemImage: "../assets/images/item-3.jpg",
+      itemImage: "assets/images/item-3.jpg",
       itemName: "Item 3",
       itemprice: "1003",
       itemCategory: "Category 3"
     },
     {
-      itemImage: "../assets/images/item-1.jpg",
+      itemImage: "assets/images/item-1.jpg",
       itemName: "Item 4",
       itemprice: "1004",
       itemCategory: "Category 4"
     },
     {
-      itemImage: "../assets/images/item-2.jpg",
+      itemImage: "assets/images/item-2.jpg",
       itemName: "Item 5",
       itemprice: "1005",
       itemCategory: "Category 5"
     },
     {
-      itemImage: "../assets/images/item-3.jpg",
+      itemImage: "assets/images/item-3.jpg",
       itemName: "Item 6",
       itemprice: "1006",
       itemCategory: "Category 6"
     },
     {
-      itemImage: "../assets/images/item-1.jpg",
+      itemImage: "assets/images/item-1.jpg",
       itemName: "Item 7",
       itemprice: "1007",
       itemCategory: "Category 7"
     },
     {
-      itemImage: "../assets/images/item-2.jpg",
+      itemImage: "assets/images/item-2.jpg",
       itemName: "Item 8",
       itemprice: "1008",
       itemCategory: "Category 8"
     },
     {
-      itemImage: "../assets/images/item-3.jpg",
+      itemImage: "assets/images/item-3.jpg",
       itemName: "Item 9",
       itemprice: "1009",
       itemCategory: "Category 9"
     },
     {
-      itemImage: "../assets/images/item-1.jpg",
+      itemImage: "assets/images/item-1.jpg",
       itemName: "Item 10",
       itemprice: "10010",
       itemCategory: "Category 10"
